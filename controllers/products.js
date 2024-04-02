542e0d85ab81c118e39a6149aded4839388e3a52
gouribhise <gouri.a.bhise@gmail.com>
2024-04-02 18:05:21 +0530

    completed simple store products api project

--- controllers/products.js
+++ controllers/products.js
@@ -5,7 +5,7 @@ const getAllProductsStatic=async(req,res)=>{
 }
 
 const getAllProducts=async(req,res)=>{
-    const{featured,company,name,sort}=req.query
+    const{featured,company,name,sort,fields,numericFilters}=req.query
     const queryObject={}
     if(featured){
         queryObject.featured=featured==='true'?true:false
@@ -17,14 +17,52 @@ const getAllProducts=async(req,res)=>{
     if(name){
         queryObject.name={$regex:name,$options:'i'}
     }
+
+    if(numericFilters){
+        const operatorMap={
+            '>':'$gt',
+            '>=':'$gte',
+            '=':'$eq',
+            '<':'$lt',
+            '<=':'$lte',
+
+        }
+        const regEx=/\b(<|>|>=|=|<|<=)\b/g
+        let filters=numericFilters.replace(regEx,(match)=>`-${operatorMap[match]}-`)
+        const options=['price','rating']
+        filters=filters.split(',').forEach((item)=>{
+            //price-$gt-40,rating-$gte-4
+
+            const[field,operator,value]=item.split('-')    
+            if(options.includes(field)){
+                queryObject[field]={[operator]:Number(value)}
+            }
+        })
+    }
+    console.log(queryObject)
+
     let result=Product.find(queryObject)
+    //sort
     if(sort){
-       // products=products.sort()
+ 
        const sortList=sort.split(',').join(' ')
        result=result.sort(sortList)
     }else{
         result=result.sort('createAt')
     }
+
+    if(fields){
+        const fieldsList=fields.split(',').join(' ')
+       result=result.select(fieldsList)
+    }
+//pagination
+
+const page=Number(req.query.page)||1
+const limit=Number(req.query.limit)||10
+//when user will be on second page skip=(2-1)*limit so it will skip first 10 / chosen limit and then add next
+const skip=(page-1)*limit
+
+result=result.skip(skip).limit(limit)
 const products=await result
 
     res.status(200).json({products,nbHits:products.length})
